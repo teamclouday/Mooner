c8cf3e15c9276e69c6e12ef365867a4be29bbe31
teamclouday <teamclouday@gmail.com>
2020-04-01 02:34:42 -0400

    update

--- fetch.py
+++ fetch.py
@@ -7,6 +7,7 @@ import os
 import json
 import time
 import tweepy
+import pickle
 import networkx as nx
 
 class TwitterAPI:
@@ -28,7 +29,7 @@ class TwitterAPI:
         self.remaining_time = 0 if self.remaining_time <= 0 else self.remaining_time
 
 class Crawler:
-    def __init__(self, auth_path="auth.json", max_nodes=100000, max_followers_per_user=5000, max_friends_per_user=5000):
+    def __init__(self, auth_path="auth.json", max_nodes=100000, max_followers_per_user=5000, max_friends_per_user=5000, keep_cache=False):
         self.auth_path = auth_path
         self.api_queue = []
         self.start = None
@@ -37,6 +38,9 @@ class Crawler:
         self.max_followers_per_user = max_followers_per_user
         self.max_friends_per_user = max_friends_per_user
         self.cursor_saved = -1 # for storing temp cursor
+        self.nodes_in_search = []
+        self.search_index = -1
+        self.keep_cache = keep_cache
 
     def set_starting_user(self, username=None, userid=None):
         """Set the starting point"""
@@ -68,35 +72,36 @@ class Crawler:
     def recycle_apis(self):
         """Put the current api to the end of queue, and start using the next one"""
         assert len(self.api_queue) >= 1
+        self._refresh()
         first = self.api_queue[0]
-        self.api_queue = self.api_queue[1:].append(first)
+        self.api_queue = self.api_queue[1:] + [first]
         return self.api_queue[0].remaining_time
 
-    def refresh(self):
+    def _refresh(self):
         """Refresh all api block time"""
         for api in self.api_queue:
             api.update()
 
-    def start(self):
+    def run(self, savefile=True):
         """Start crawling"""
         assert self.start is not None
         assert len(self.api_queue) >= 1
-        nodes_in_search = [self.start]
+        self.nodes_in_search = [self.start] if len(self.nodes_in_search) <= 0 else self.nodes_in_search
         while self.graph.number_of_nodes() < self.max_nodes:
-            index = 0
+            self.search_index = 0 if self.search_index <= 0 else self.search_index
             new_nodes_in_search = []
-            while index < len(nodes_in_search):
-                nodeid = nodes_in_search[index]
+            while (self.search_index < len(self.nodes_in_search)) and (self.graph.number_of_nodes() < self.max_nodes):
+                nodeid = self.nodes_in_search[self.search_index]
                 print("Currently looking at id={}".format(nodeid))
                 try:
                     follower_ids = self.api_queue[0].api.followers_ids(user_id=nodeid)
                     friends_ids = self.api_queue[0].api.friends_ids(user_id=nodeid)
                 except tweepy.RateLimitError:
-                    self.refresh()
                     self.api_queue[0].block() # block current api
                     remaining = self.recycle_apis() # recycle apis
                     if(remaining > 0): # if all apis are blocked, then wait
-                        print("All apis are blocked, sleeping for {}s".format(remaining))
+                        print("All APIs are blocked, sleeping for {:.2f}s".format(remaining))
+                        self._tmp_save()
                         time.sleep(remaining + 1)
                 else:
                     for followerid in follower_ids:
@@ -107,13 +112,30 @@ class Crawler:
                         if not self.graph.has_node(friendid):
                             new_nodes_in_search.append(friendid)
                         self.graph.add_edge(nodeid, friendid)
-                    index += 1
+                    self.search_index += 1
                     print("Retrieve complete - Current graph size: {}".format(self.graph.number_of_nodes()))
                     time.sleep(1)
-            nodes_in_search = new_nodes_in_search
+            self.nodes_in_search = new_nodes_in_search
+            self.search_index = -1
             time.sleep(1)
+        print("Crawling Complete - Total Number of Nodes: {}".format(self.graph.number_of_nodes()))
+        if savefile:
+            print("Saving edge list")
+            with open("fetchcontent.edgelist", "wb") as outFile:
+                nx.write_edgelist(self.graph, outFile)
+        if not self.keep_cache:
+            self._tmp_delete()
+
+    def _tmp_save(self):
+        with open("fetch.py.tmp.pickle", "wb") as outFile:
+            pickle.dump(self, outFile)
+
+    def _tmp_delete(self):
+        if os.path.exists("fetch.py.tmp.pickle"):
+            os.remove("fetch.py.tmp.pickle")
 
 if __name__ == "__main__":
-    app = Crawler(auth_path=os.path.join("auth.json"))
+    app = Crawler(auth_path=os.path.join("auth.json"), keep_cache=True, max_nodes=10000)
     app.load_auths()
-    app.set_starting_user()
+    app.set_starting_user(username="3blue1brown")
+    app.run()
