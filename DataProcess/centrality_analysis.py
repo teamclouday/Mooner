12f87845126588fca14f2290ea670aceaedd3205
teamclouday <teamclouday@gmail.com>
2020-04-08 15:56:54 -0400

    update centrality algo

--- DataProcess/centrality_analysis.py
+++ DataProcess/centrality_analysis.py
@@ -1,4 +1,5 @@
 import os
+import math
 import itertools
 import pandas as pd
 import networkx as nx
@@ -9,15 +10,18 @@ communities = pd.read_csv("community.csv")
 centrality = []
 counter = 0
 
+ratio = 0.2
+
 for subgraphlist in communities["Members"]:
     subgraphnodes = [int(x) for x in subgraphlist.split(" | ")]
     subgraph = graph.subgraph(subgraphnodes)
     totalcentra = nx.degree_centrality(subgraph)
     totalcentra = [[k, v] for k, v in sorted(totalcentra.items(), key=lambda item: item[1])]
-    result = list(reversed(totalcentra))[:5]
+    result_len = math.ceil(len(totalcentra) * ratio)
+    result = list(reversed(totalcentra))[:result_len]
     result_str = " | ".join([str(x) for (x, y) in result])
     centrality.append(result_str)
-    print("Community {} top 5: {}".format(counter, result))
+    print("Community {}: {}".format(counter, result))
     counter += 1
 
 df = pd.DataFrame(centrality, columns=["Centers ID"])
